88af3bab63b0a132734f50c1c31c2475429847be
Apple <purushottam.7922@gmail.com>
2022-04-26 18:51:55 +0530

    changes

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ let i = 0;
 let ply1score = 0;
 let ply2score = 0;
 let a;
+let gameEnd = false
 function rollDice(){
     a = Math.floor(Math.random()*6) + 1;
-    if(i%2===0){
-        ply1score = ply1score + a;
-        document.getElementById("ply1").value = ply1score;
-        if(ply1score >= threshold){
-            document.getElementById("result").innerHTML = "Player 1 wins";
+    if(!gameEnd){
+        if(i%2===0){
+            ply1score = ply1score + a;
+            document.getElementById("ply1").value = ply1score;
+            if(ply1score >= threshold){
+                document.getElementById("result").innerHTML = "Player 1 wins";
+                gameEnd = true
+                return;
+            }
         }
-    }
-    else{
-        ply2score = ply2score + a;
-        document.getElementById("ply2").value = ply2score;
-        if(ply2score >= threshold){
-            document.getElementById("result").innerHTML = "Player 2 wins";
+        else{
+            ply2score = ply2score + a;
+            document.getElementById("ply2").value = ply2score;
+            if(ply2score >= threshold){
+                document.getElementById("result").innerHTML = "Player 2 wins";
+                gameEnd = true
+                return;
+            }
         }
+        i = i+1;
     }
-    i = i+1;
 }
